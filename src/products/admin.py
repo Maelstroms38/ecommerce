8803bd4d683a05441b0d3e278815985fcd2554f1
Maelstroms38 <stroms38@gmail.com>
2015-12-21 14:29:01 -0500

    product categories

--- src/products/admin.py
+++ src/products/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Product, Example, Topic, Concept, Answer, Variation, ProductImage
+from .models import Product, Example, Topic, Concept, Answer, Variation, ProductImage, Category
 
 admin.site.register(Product)
 admin.site.register(Example)
@@ -11,3 +11,4 @@ admin.site.register(Concept)
 admin.site.register(Answer)
 admin.site.register(Variation)
 admin.site.register(ProductImage)
+admin.site.register(Category)
--- src/products/models.py
+++ src/products/models.py
@@ -20,9 +20,10 @@ class Product(models.Model):
 	description = models.TextField(blank=True, null=True)
 	price = models.DecimalField(decimal_places=2, max_digits=1000)
 	active = models.BooleanField(default=True)
-	#decription
-	#active, answered
-	#slug
+
+        categories = models.ManyToManyField('Category', blank=True)
+        default = models.ForeignKey('Category', related_name='default_category', null=True, blank=True)
+    
 	objects = ProductManager()
 
 	def __unicode__(self):
@@ -77,6 +78,15 @@ class ProductImage(models.Model):
     def __unicode__(self):
         return self.product.title
 # Images, Categories
+class Category(models.Model):
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(null=True, blank=True)
+    active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __unicode__(self):
+        return self.title
 
 class Topic(models.Model):
     """
